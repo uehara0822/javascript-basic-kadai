8d66a1e184eb4b3faccfb0f6849ae5cfc983ce84
uehara0822 <166109323+uehara0822@users.noreply.github.com>
2024-04-19 08:30:10 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+const moonLanding = new Date();
+
+
+const yearMonthDate=()=>{
+console.log(moonLanding.getFullYear()+'年'+(moonLanding.getMonth()+1)+'月'+moonLanding.getDate()+'日');
+}
+
+
+yearMonthDate();
+
+  
+
